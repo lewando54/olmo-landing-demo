fe93d2d0de08198793f665e5aadeb2cc45936f69
Jakub Lewandowski <lewando54@gmail.com>
2022-08-22 12:47:35 +0200

    fast

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 const el = document.querySelector(".header")
 const observer = new IntersectionObserver( 
-  (e) => {
+  ([e]) => {
     document.querySelector('.header--nav').classList.toggle("header--nav__is-pinned", e.intersectionRatio < 1);
-    e.target.classList.toggle("header__is-pinned", e.intersectionRatio < 1)
+    [e].target.classList.toggle("header__is-pinned", e.intersectionRatio < 1)
   },
   { threshold: 1 }
 );
